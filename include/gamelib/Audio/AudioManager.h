b7a00f68712a614bb4e5bc354e6592d0df9eaecc
Ryan Manning <ryan2.manning@live.uwe.ac.uk>
2020-03-27 19:10:58 +0000

    started to add audio
    need to thread it also need to work out what to do about the setup

---
+++ include/gamelib/Audio/AudioManager.h
@@ -0,0 +1,30 @@
+//
+// Created by Ryan on 27/03/2020.
+//
+
+#ifndef MYNETGAME_AUDIOMANAGER_H
+#define MYNETGAME_AUDIOMANAGER_H
+
+#include <Engine/FileIO.h>
+#include <Engine/OGLGame.h>
+#include <soloud.h>
+#include <soloud_wav.h>
+
+class AudioManager
+{
+ public:
+  AudioManager() = default;
+  ~AudioManager();
+
+  AudioManager(const AudioManager& audio){};
+  AudioManager& operator=(AudioManager audio);
+
+  bool audioSetUp();
+
+ private:
+  ASGE::FILEIO::File click_004;
+  SoLoud::Wav click_004_MP3;
+
+  SoLoud::Soloud soloud;
+};
+#endif  // MYNETGAME_AUDIOMANAGER_H
---
+++ source/Audio/AudioManager.cpp
@@ -0,0 +1,41 @@
+//
+// Created by Ryan on 27/03/2020.
+//
+
+#include "Audio/AudioManager.h"
+
+AudioManager::~AudioManager()
+{
+  soloud.deinit();
+}
+
+AudioManager& AudioManager::operator=(const AudioManager audio)
+{
+  if (&audio != this)
+  {
+    // delete pointers
+  }
+
+  return *this;
+}
+
+bool AudioManager::audioSetUp()
+{
+  if (soloud.init() != SoLoud::SO_NO_ERROR)
+  {
+    return false;
+  }
+
+  if (click_004.open("/data/Audio/click_004.mp3"))
+  {
+    auto io_buffer = click_004.read();
+    // if(click_004_MP3.loadMem(io_buffer.as_unsigned_char(), static_cast<unsigned
+    // int>(io_buffer.length), false,false)) click_004_MP3.loadMem(io_buffer.as_unsigned_char(),
+    // static_cast<unsigned int>(io_buffer.length), false,false);
+    click_004.close();
+  }
+  else
+  {
+    return false;
+  }
+}
