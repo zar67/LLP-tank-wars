7b41f340c51d73df6546326cb9344915f752916f
Zoe <zoerowbotham1@gmail.com>
2020-04-25 19:19:24 +0100

    Travis

--- include/gamelib/ui/scenes/Shop.h
+++ include/gamelib/ui/scenes/Shop.h
@@ -33,10 +33,11 @@ class Shop
   std::vector<Button*> units         = {};
   std::vector<ASGE::Text*> cost_text = {};
 
-  const std::vector<std::string> unit_types{"tank.png",
-                                            "tank_big.png",
-                                            "tank_large.png",
-                                            "tank_huge.png"};
+  const std::vector<std::string> unit_types{
+    "tank.png",
+    "tank_big.png",
+    "tank_large.png",
+    "tank_huge.png"};
 
   const std::vector<std::string> unit_costs{"5", "15", "30", "50"};
   std::array<int, 2> local_cam_pos = {0, 0};
@@ -45,10 +46,11 @@ class Shop
 
   std::vector<ASGE::Sprite*> unit_stats_ui{};
 
-  const std::vector<std::string> unit_data{"5\n15\n5\n3\n1",
-                                           "15\n20\n10\n2\n2",
-                                           "30\n30\n20\n2\n2",
-                                           "50\n40\n30\n1\n3"};
+  const std::vector<std::string> unit_data{
+    "5\n15\n5\n3\n1",
+    "15\n20\n10\n2\n2",
+    "30\n30\n20\n2\n2",
+    "50\n40\n30\n1\n3"};
 };
 
 #endif  // MYNETGAME_SHOP_H
